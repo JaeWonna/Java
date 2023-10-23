81586d0b60b2c3573597e111dcc5c5e826a7e1c5
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-10-23 21:08:00 +0900

    Create this()로 다른 생성자 호출.java

---
+++ 명품 Java Programming/Review/Chapter4. 클래스와 객체/this()로 다른 생성자 호출.java
@@ -0,0 +1,38 @@
+package Chapter4_Review;
+
+class Book{
+	String title;
+	String author;
+	
+	void show() {
+		System.out.println(title + " " + author);
+	}
+	
+	public Book() {
+		this("","");
+		System.out.println("생성자 호출됨");
+	}
+	
+	public Book(String title) {
+		this(title, "작자미상");
+	}
+
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+}
+
+public class BookEx {
+	public static void main(String[] args) {
+		Book littlePrince = new Book("어린왕자","생텍쥐페리");
+		Book loveStory = new Book("춘향전");
+		Book emptyBook = new Book();
+		loveStory.show();
+	}
+}
+
+
+
+
+
