19fa8a627648dcc5fe6bb6fc6390f8e17fe2ff3c
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-11-26 15:35:12 +0900

    Create (throws) 예외가 발생한 메소드를 호출한 곳으로 예외 객체를 넘기는 방법.java

---
+++ 자바 Study/기본 복습/(throws) 예외가 발생한 메소드를 호출한 곳으로 예외 객체를 넘기는 방법.java
@@ -0,0 +1,21 @@
+package java_package;
+
+public class practice {
+    
+    static void callDriver() throws ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        System.out.println("완료");
+    }
+    
+    public static void main(String[] args){
+        try{
+            callDriver();
+        } catch (ClassNotFoundException e){
+            System.out.println("클래스를 찾을 수 없습니다");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("시스템 종료");
+        }
+    }
+}
