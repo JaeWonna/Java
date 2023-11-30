539a1e610de8ed0014c0a638f301463e2b06b326
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-11-30 15:10:27 +0900

    Create 2. 클래스에서 스트림 사용.java

---
+++ 자바 Study/기본 복습/스트림/2. 클래스에서 스트림 사용.java
@@ -0,0 +1,43 @@
+package java_package;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
+
+public class practice {
+    public static void main(String[] args){
+        List<Student> studentList = Arrays.asList(
+                new Student("홍길동",10),
+                new Student("이순신",20),
+                new Student("임꺽정",30)
+        );
+
+        Stream<Student> stream = studentList.stream();
+        stream.forEach(s -> System.out.println("이름 : " + s.getName() + " 점수 : " + s.getScore()));
+    }
+}
+
+
+
+
+
+
+
