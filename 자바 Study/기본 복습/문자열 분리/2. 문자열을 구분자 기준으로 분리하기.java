589162818c4139ae3efba525c75c293a73b9a09b
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-11-30 14:54:00 +0900

    Create 2. 문자열을 구분자 기준으로 분리하기.java

---
+++ 자바 Study/기본 복습/문자열 분리/2. 문자열을 구분자 기준으로 분리하기.java
@@ -0,0 +1,21 @@
+package java_package;
+
+import java.util.StringTokenizer;
+
+public class practice {
+    public static void main(String[] args){
+        String str = "안녕하세요!오늘!날씨는!맑음";
+        StringTokenizer st = new StringTokenizer(str,"!");
+
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
+
+
+
+
+
+
+
